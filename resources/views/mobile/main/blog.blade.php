d85e234112b5e9cbe8ed71cffdcbf3b83d3fba04
avicha <avichabc@gmail.com>
2016-07-15 18:01:49 +0800

    react为调试模式

--- resources/views/mobile/main/blog.blade.php
+++ resources/views/mobile/main/blog.blade.php
@@ -20,7 +20,7 @@
 
 @section('javascript')
 <script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/lib/react/15.1.0/react-dev.js"></script>
-<script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/lib/react/15.1.0/react-dom.js"></script>
+<script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/lib/react/15.1.0/react-dom-dev.js"></script>
 <script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/lib/redux/3.5.2/redux.js"></script>
 <script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/lib/react-redux/4.4.5/react-redux.js"></script>
 <script type="text/javascript" src="{{ config('app.static_url_prefix') }}/js/mobile/main/app.bundle.js"></script>
