cf2db98f32dfc93ca4f9ccaf545d235eb1347118
avicha <avichabc@gmail.com>
2016-07-19 15:39:38 +0800

    增加了lazypipe包

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var gulpif = require('gulp-if');
+var lazypipe = require('lazypipe');
 var del = require('del');
 var webpack = require('webpack');
 //CSS
